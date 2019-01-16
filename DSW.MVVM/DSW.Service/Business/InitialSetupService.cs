d1f1f19aa3c6a163e3b3245adf9ac31252f74d14
d.sukma <d.sukma@fsoft.com.vn>
2019-01-16 21:12:57 +0700

    Update framework

--- DSW.MVVM/DSW.Service/Business/InitialSetupService.cs
+++ DSW.MVVM/DSW.Service/Business/InitialSetupService.cs
@@ -2,14 +2,80 @@
 using System.Linq;
 using System.Collections.Generic;
 using System.Text;
+using DSW.Core.Utility.Services;
+using DSW.Database;
+using DSW.Service.ApiClient;
+using DSW.Service.ApiModels;
 
 namespace DSW.Service.Business
 {
     public interface IInitialSetupService
     {
+        ResponseResult InitialSetup();
     }
 
     public class InitialSetupService : IInitialSetupService
     {
+        private IUnitOfWork _unitOfWork;
+        private IApiClientService _apiClientService;
+
+        public InitialSetupService(
+            IUnitOfWork unitOfWork,
+            IApiClientService apiClientService
+        )
+        {
+            this._unitOfWork = unitOfWork;
+            this._apiClientService = apiClientService;
+        }
+
+        #region IInitialSetupService Members
+
+        public ResponseResult InitialSetup()
+        {
+            return ResponseResult.Succeed();
+        }
+
+        #endregion
+
+        #region Private Methods
+
+        private InitialSetupApiResponseModel CallApiInitialSetup()
+        {
+            try
+            {
+                var response = new InitialSetupApiResponseModel();
+
+                return response;
+            }
+            catch (Exception ex)
+            {
+                return null;
+            }
+        }
+
+        private bool InsertMstItemToDB()
+        {
+            try
+            {
+                return true;
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+        }
+
+        private bool InsertAppSettingToDB()
+        {
+            try
+            {
+                return true;
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+        }
+        #endregion
     }
 }
